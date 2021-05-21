c36dcd3d2d7f201905e331b1b9909eca13d5520d
Kylin24Kwok <1296845690@qq.com>
2021-05-21 21:51:29 +0800

    加入密码修改弹窗

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -14,10 +14,55 @@
       :discussion="discussion"
     ></DiscussionCard>
 
-    <v-row justify="space-around" style="margin: 20px 0; width =100%"
-      ><v-btn color="primary" width="40%" @click="changePassWd">
-        修改密码
-      </v-btn>
+    <v-row justify="space-around" style="margin: 20px 0; width =100%">
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" width="40%" v-bind="attrs" v-on="on">
+            修改密码
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">修改密码</span>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-model="newPassWd"
+                :rules="passwordRules"
+                label="新密码"
+                required
+              ></v-text-field>
+
+              <v-row
+                align="center"
+                justify="center"
+                style="margin-bottom: -12px"
+              >
+                <v-col cols="6" sm="8">
+                  <v-text-field
+                    v-model="code"
+                    label="邮件验证码"
+                    :counter="6"
+                    :rules="codeRules"
+                  />
+                </v-col>
+                <v-col cols="6" sm="4">
+                  <v-btn
+                    color="primary"
+                    block
+                    :disabled="!sendValid"
+                    @click="sendCode"
+                  >
+                    {{ sendButton }}
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
       <v-btn color="error" width="40%" @click="logout">退出登录</v-btn></v-row
     >
   </v-container>
@@ -33,9 +78,53 @@ export default {
   data() {
     return {
       profile: {},
+      showPassWdDialog: false,
+      newPassWd: '',
+      codeRules: [
+        (v) => !!v || '内容不能为空',
+        (v) => /^[0-9]{6}$/.test(v) || '验证码格式不对',
+      ],
+      passwordRules: [
+        (v) => !!v || '内容不能为空',
+        (v) => v.length <= 32 || '密码不能超过32字符',
+        (v) => v.length >= 8 || '密码不能少于8字符',
+      ],
+      sendButton: '发送验证码',
+      sendValid: true,
     }
   },
   methods: {
+    sendCode() {
+      this.sendButtonChangeStatus()
+      this.$refs.message.info('验证码已发送, 请检查邮件以继续')
+      this.$axios
+        .get('register/', {
+          params: {
+            username: this.username,
+            email: this.email,
+            usage: 'change_password',
+          },
+        })
+        .then((response) => {
+          if (response.data.data !== 0) {
+            this.$refs.message.error(response.data.msg)
+          } else {
+            this.$refs.message.success(response.data.msg)
+          }
+        })
+    },
+    sendButtonChangeStatus() {
+      this.sendValid = false
+      for (let i = 60; i >= 0; i--) {
+        setTimeout(() => {
+          this.sendButton = i
+          if (this.sendButton === 0) {
+            this.sendButton = '发送验证码'
+            this.sendValid = true
+          }
+        }, 1000 * (60 - i))
+      }
+    },
     getUserInfo() {
       this.$axios
         .get('/profile/')
@@ -52,6 +141,7 @@ export default {
       this.$feUtils.reloadAll()
       console.log('2222222222222222')
     },
+    getEmailAuthCode() {},
     changePassWd() {
       alert('还没写完')
     },
